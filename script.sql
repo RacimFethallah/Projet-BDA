112ee0c6838249d5725cd493c40a5d944a275c90
RacimFethallah <RacimFethallah@proton.me>
2024-04-03 02:05:24 +0100

    Ajouter des types d'objet et des tables pour les clients, les employés, les marques, les modèles, les véhicules, les interventions et les intervenants.

--- script.sql
+++ script.sql
@@ -30,8 +30,8 @@ GRANT ALL PRIVILEGES TO SQL3_BDA;
 
 -- C. Langage de definition de donnees
 -- 5. definir tous les types necessaires.
-
-CREATE OR REPLACE TYPE tclient AS OBJECT(
+--done
+CREATE OR REPLACE TYPE tclient AS OBJECT(  
     numclient INTEGER,
     civ VARCHAR2(3), 
     prenomclient VARCHAR2(50), 
@@ -44,10 +44,100 @@ CREATE OR REPLACE TYPE tclient AS OBJECT(
 );
 /
 
+--done
+CREATE OR REPLACE TYPE temploye AS OBJECT(
+    numemploye INTEGER,
+    nomemploye VARCHAR2(50),
+    prenomemploye VARCHAR2(50),
+    categorie VARCHAR2(50),
+    salaire float
+);
+/
+
+
+--done
+CREATE OR REPLACE TYPE tmarque AS OBJECT(
+    NUMMARQUE INTEGER,
+    MARQUE VARCHAR2(50),
+    PAYS VARCHAR2(50)
+);
+/
+
+/* jsp si on utilise ça
+Create type tset_ref_marque as table of ref tville;
+/ 
+*/
+
+
+CREATE OR REPLACE TYPE tmodele AS OBJECT(
+    NUMMODELE INTEGER,
+    NUMMARQUE INTEGER,
+    MODELE VARCHAR2(50)
+);
+/
+
+CREATE OR REPLACE TYPE tvehicule AS OBJECT(
+    NUMVEHICULE INTEGER,
+    NUMCLIENT INTEGER,
+    NUMMODELE INTEGER,
+    NUMIMMAT VARCHAR2(50),
+    ANNEE INTEGER
+);
+/
+
+CREATE OR REPLACE TYPE tinterventions AS OBJECT(
+    NUMINTERVENTION INTEGER,
+    NUMVEHICULE INTEGER,
+    TYPEINTERVENTION VARCHAR2(50),
+    DATEDEBINTERV DATE,
+    DATEFININTERV DATE,
+    COUTINTERV NUMBER
+);
+/
+
+CREATE OR REPLACE TYPE tintervenants AS OBJECT(
+    NUMINTERVENTION INTEGER,
+    NUMEMPLOYE INTEGER,
+    DATEDEBUT DATE,
+    DATEFIN DATE
+);
+/
+
+
+
 --pour apres
 CREATE TABLE client OF tclient (
     CONSTRAINT numclient_pk PRIMARY KEY (numclient),
     CONSTRAINT civ_check CHECK (civ IN ('M', 'Mle', 'Mme'))
 );
 
+CREATE TABLE employe OF temploye (
+    CONSTRAINT numemploye_pk PRIMARY KEY (NUMEMPLOYE)
+);
 
+CREATE TABLE marque OF tmarque (
+    CONSTRAINT nummarque_pk PRIMARY KEY (NUMMARQUE)
+);
+
+CREATE TABLE modele OF tmodele (
+    CONSTRAINT nummodele_pk PRIMARY KEY (NUMMODELE),
+    CONSTRAINT nummarque_fk FOREIGN KEY (NUMMARQUE) REFERENCES marque(NUMMARQUE)
+);
+
+CREATE TABLE vehicule OF tvehicule (
+    CONSTRAINT numvehicule_pk PRIMARY KEY (NUMVEHICULE),
+    CONSTRAINT numclient_fk FOREIGN KEY (NUMCLIENT) REFERENCES client(NUMCLIENT),
+    CONSTRAINT nummodele_fk FOREIGN KEY (NUMMODELE) REFERENCES modele(NUMMODELE)
+);
+
+CREATE TABLE interventions OF tinterventions (
+    CONSTRAINT numintervention_pk PRIMARY KEY (NUMINTERVENTION),
+    CONSTRAINT numvehicule_fk FOREIGN KEY (NUMVEHICULE) REFERENCES vehicule(NUMVEHICULE)
+);
+
+CREATE TABLE intervenants OF tintervenants (
+    CONSTRAINT numintervention_fk FOREIGN KEY (NUMINTERVENTION) REFERENCES interventions(NUMINTERVENTION),
+    CONSTRAINT numemploye_fk FOREIGN KEY (NUMEMPLOYE) REFERENCES employe(NUMEMPLOYE)
+);
+
+-- D. Langage de manipulation de donnees
